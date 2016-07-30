2aa6b16fc49d6c8ee7b17fe0749cf70519bded6d
吴聪 <1875486458@qq.com>
2016-07-30 14:06:44 +0800

    第8天:增加原生Java函数调用支持

---
+++ src/com/misakimei/stone/Native.java
@@ -0,0 +1,58 @@
+package com.misakimei.stone;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by 18754 on 2016/7/30.
+ */
+public class Native {
+    public Environment environment(Environment env) {
+        appendNative(env);
+        return env;
+    }
+
+    protected void appendNative(Environment env) {
+        append(env, "print", Native.class, "print", Object.class);
+        append(env, "length", Native.class, "length", String.class);
+        append(env, "toInt", Native.class, "toInt", Object.class);
+        append(env, "currentTime", Native.class, "currentTime");
+    }
+
+    //具体的增加原生函数的方法
+    //将clazz类中的名字叫name参数是params的函数加到env中 名字是methodname
+    private void append(Environment env, String name, Class<?> clazz, String methodname, Class<?>... params) {
+        Method m = null;
+        try {
+            m = clazz.getMethod(methodname, params);
+
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        env.put(name, new NativeFunction(methodname, m));//预先将NativeFunction加入环境中
+    }
+
+    public static int print(Object obj) {
+        System.out.println("==>"+obj.toString());
+        return 0;
+    }
+
+    public static int length(String s) {
+        return s.length();
+    }
+
+    public static int toInt(Object val) {
+        if (val instanceof String) {
+            return Integer.parseInt((String) val);
+        } else if (val instanceof Integer) {
+            return ((Integer) val).intValue();
+        } else {
+            throw new NumberFormatException(val.toString());
+        }
+    }
+
+
+    public static int currentTime() {
+        return (int) (System.currentTimeMillis());
+    }
+
+}
---
+++ src/com/misakimei/stone/NativeFunction.java
@@ -0,0 +1,37 @@
+package com.misakimei.stone;
+
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by 18754 on 2016/7/30.
+ */
+public class NativeFunction{
+
+    protected String name;
+    protected Method method;
+    protected int   numparam;
+    public NativeFunction(String name, Method m){
+        this.name=name;
+        this.method=m;
+        numparam=m.getParameterTypes().length;//方法所需的参数的长度
+    }
+    public int numParams() {
+        return numparam;
+    }
+
+    public Object invoke(Object[] arg, Arguments arguments) {
+        try {
+         return    method.invoke(null,arg);
+        } catch (Exception e){
+            throw new StoneExcetion("调用原生JAVA函数"+name+"失败",arguments);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "<NativeFunction "+hashCode()+">";
+    }
+}
